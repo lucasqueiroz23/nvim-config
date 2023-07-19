2ceddd637738b227f0baeb28e49ba4a0b3b3388a
Lucas <lucas.fn4@gmail.com>
2023-07-19 09:28:06 -0300

    Remove LSP (will config it again from scratch)

--- lua/lucasqueiroz/packer.lua
+++ lua/lucasqueiroz/packer.lua
@@ -39,26 +39,6 @@ return require('packer').startup(function(use)
   -- vim fugitive(manipulate git stuff)
   use 'tpope/vim-fugitive'
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v2.x',
-	  requires = {
-		-- LSP Support
-		{'neovim/nvim-lspconfig'},             -- Required
-		{                                      -- Optional
-		  'williamboman/mason.nvim',
-		  run = function()
-			pcall(vim.cmd, 'MasonUpdate')
-		  end,
-		},
-		{'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-		-- Autocompletion
-		{'hrsh7th/nvim-cmp'},     -- Required
-		{'hrsh7th/cmp-nvim-lsp'}, -- Required
-		{'L3MON4D3/LuaSnip'},     -- Required
-	  }
-}
   use 'jiangmiao/auto-pairs'
 
 end)
